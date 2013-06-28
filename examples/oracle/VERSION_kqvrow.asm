6712f40f426387be5ed2c687a387b830ec5cfa2d
Dennis Yurichev <dennis@yurichev.com>
2013-06-29 02:12:34 +0300

    =3 oracle examples added

---
+++ examples/oracle/VERSION_kqvrow.asm
@@ -0,0 +1,207 @@
+_kqvrow_        proc near
+
+var_7C          = byte ptr -7Ch
+var_18          = dword ptr -18h
+var_14          = dword ptr -14h
+Dest            = dword ptr -10h
+var_C           = dword ptr -0Ch
+var_8           = dword ptr -8
+var_4           = dword ptr -4
+arg_8           = dword ptr  10h
+arg_C           = dword ptr  14h
+arg_14          = dword ptr  1Ch
+arg_18          = dword ptr  20h
+
+; FUNCTION CHUNK AT .text1:056C11A0 SIZE 00000049 BYTES
+
+                push    ebp
+                mov     ebp, esp
+                sub     esp, 7Ch
+                mov     eax, [ebp+arg_14] ; [EBP+1Ch]=1
+                mov     ecx, TlsIndex   ; [69AEB08h]=0
+                mov     edx, large fs:2Ch
+                mov     edx, [edx+ecx*4] ; [EDX+ECX*4]=0xc98c938
+                cmp     eax, 2          ; EAX=1
+                mov     eax, [ebp+arg_8] ; [EBP+10h]=0xcdfe554
+                jz      loc_2CE1288
+                mov     ecx, [eax]      ; [EAX]=0..5
+                mov     [ebp+var_4], edi ; EDI=0xc98c938
+
+loc_2CE10F6:                            ; CODE XREF: _kqvrow_+10A
+                                        ; _kqvrow_+1A9
+                cmp     ecx, 5          ; ECX=0..5
+                ja      loc_56C11C7
+                mov     edi, [ebp+arg_18] ; [EBP+20h]=0
+                mov     [ebp+var_14], edx ; EDX=0xc98c938
+                mov     [ebp+var_8], ebx ; EBX=0
+                mov     ebx, eax        ; EAX=0xcdfe554
+                mov     [ebp+var_C], esi ; ESI=0xcdfe248
+
+loc_2CE110D:                            ; CODE XREF: _kqvrow_+29E00E6
+                mov     edx, ds:off_628B09C[ecx*4] ; [ECX*4+628B09Ch]=0x2ce1116, 0x2ce11ac, 0x2ce11db, 0x2ce11f6, 0x2ce1236, 0x2ce127a
+                jmp     edx             ; EDX=0x2ce1116, 0x2ce11ac, 0x2ce11db, 0x2ce11f6, 0x2ce1236, 0x2ce127a
+; ---------------------------------------------------------------------------
+
+loc_2CE1116:                            ; DATA XREF: .rdata:off_628B09C
+                push    offset aXKqvvsnBuffer ; "x$kqvvsn buffer"
+                mov     ecx, [ebp+arg_C] ; [EBP+14h]=0x8a172b4
+                xor     edx, edx
+                mov     esi, [ebp+var_14] ; [EBP-14h]=0xc98c938
+                push    edx             ; EDX=0
+                push    edx             ; EDX=0
+                push    50h
+                push    ecx             ; ECX=0x8a172b4
+                push    dword ptr [esi+10494h] ; [ESI+10494h]=0xc98cd58
+                call    _kghalf         ; tracing nested maximum level (1) reached, skipping this CALL
+                mov     esi, ds:__imp__vsnnum ; [59771A8h]=0x61bc49e0
+                mov     [ebp+Dest], eax ; EAX=0xce2ffb0
+                mov     [ebx+8], eax    ; EAX=0xce2ffb0
+                mov     [ebx+4], eax    ; EAX=0xce2ffb0
+                mov     edi, [esi]      ; [ESI]=0xb200100
+                mov     esi, ds:__imp__vsnstr ; [597D6D4h]=0x65852148, "- Production"
+                push    esi             ; ESI=0x65852148, "- Production"
+                mov     ebx, edi        ; EDI=0xb200100
+                shr     ebx, 18h        ; EBX=0xb200100
+                mov     ecx, edi        ; EDI=0xb200100
+                shr     ecx, 14h        ; ECX=0xb200100
+                and     ecx, 0Fh        ; ECX=0xb2
+                mov     edx, edi        ; EDI=0xb200100
+                shr     edx, 0Ch        ; EDX=0xb200100
+                movzx   edx, dl         ; DL=0
+                mov     eax, edi        ; EDI=0xb200100
+                shr     eax, 8          ; EAX=0xb200100
+                and     eax, 0Fh        ; EAX=0xb2001
+                and     edi, 0FFh       ; EDI=0xb200100
+                push    edi             ; EDI=0
+                mov     edi, [ebp+arg_18] ; [EBP+20h]=0
+                push    eax             ; EAX=1
+                mov     eax, ds:__imp__vsnban ; [597D6D8h]=0x65852100, "Oracle Database 11g Enterprise Edition Release %d.%d.%d.%d.%d %s"
+                push    edx             ; EDX=0
+                push    ecx             ; ECX=2
+                push    ebx             ; EBX=0xb
+                mov     ebx, [ebp+arg_8] ; [EBP+10h]=0xcdfe554
+                push    eax             ; EAX=0x65852100, "Oracle Database 11g Enterprise Edition Release %d.%d.%d.%d.%d %s"
+                mov     eax, [ebp+Dest] ; [EBP-10h]=0xce2ffb0
+                push    eax             ; EAX=0xce2ffb0
+                call    ds:__imp__sprintf ; op1=MSVCR80.dll!sprintf tracing nested maximum level (1) reached, skipping this CALL
+                add     esp, 38h
+                mov     dword ptr [ebx], 1
+
+loc_2CE1192:                            ; CODE XREF: _kqvrow_+FB
+                                        ; _kqvrow_+128 ...
+                test    edi, edi        ; EDI=0
+                jnz     __VInfreq__kqvrow
+                mov     esi, [ebp+var_C] ; [EBP-0Ch]=0xcdfe248
+                mov     edi, [ebp+var_4] ; [EBP-4]=0xc98c938
+                mov     eax, ebx        ; EBX=0xcdfe554
+                mov     ebx, [ebp+var_8] ; [EBP-8]=0
+                lea     eax, [eax+4]    ; [EAX+4]=0xce2ffb0, "NLSRTL Version 11.2.0.1.0 - Production", "Oracle Database 11g Enterprise Edition Release 11.2.0.1.0 - Production", "PL/SQL Release 11.2.0.1.0 - Production", "TNS for 32-bit Windows: Version 11.2.0.1.0 - Production"
+
+loc_2CE11A8:                            ; CODE XREF: _kqvrow_+29E00F6
+                mov     esp, ebp
+                pop     ebp
+                retn                    ; EAX=0xcdfe558
+; ---------------------------------------------------------------------------
+
+loc_2CE11AC:                            ; DATA XREF: .rdata:0628B0A0
+                mov     edx, [ebx+8]    ; [EBX+8]=0xce2ffb0, "Oracle Database 11g Enterprise Edition Release 11.2.0.1.0 - Production"
+                mov     dword ptr [ebx], 2
+                mov     [ebx+4], edx    ; EDX=0xce2ffb0, "Oracle Database 11g Enterprise Edition Release 11.2.0.1.0 - Production"
+                push    edx             ; EDX=0xce2ffb0, "Oracle Database 11g Enterprise Edition Release 11.2.0.1.0 - Production"
+                call    _kkxvsn         ; tracing nested maximum level (1) reached, skipping this CALL
+                pop     ecx
+                mov     edx, [ebx+4]    ; [EBX+4]=0xce2ffb0, "PL/SQL Release 11.2.0.1.0 - Production"
+                movzx   ecx, byte ptr [edx] ; [EDX]=0x50
+                test    ecx, ecx        ; ECX=0x50
+                jnz     short loc_2CE1192
+                mov     edx, [ebp+var_14]
+                mov     esi, [ebp+var_C]
+                mov     eax, ebx
+                mov     ebx, [ebp+var_8]
+                mov     ecx, [eax]
+                jmp     loc_2CE10F6
+; ---------------------------------------------------------------------------
+
+loc_2CE11DB:                            ; DATA XREF: .rdata:0628B0A4
+                push    0
+                push    50h
+                mov     edx, [ebx+8]    ; [EBX+8]=0xce2ffb0, "PL/SQL Release 11.2.0.1.0 - Production"
+                mov     [ebx+4], edx    ; EDX=0xce2ffb0, "PL/SQL Release 11.2.0.1.0 - Production"
+                push    edx             ; EDX=0xce2ffb0, "PL/SQL Release 11.2.0.1.0 - Production"
+                call    _lmxver         ; tracing nested maximum level (1) reached, skipping this CALL
+                add     esp, 0Ch
+                mov     dword ptr [ebx], 3
+                jmp     short loc_2CE1192
+; ---------------------------------------------------------------------------
+
+loc_2CE11F6:                            ; DATA XREF: .rdata:0628B0A8
+                mov     edx, [ebx+8]    ; [EBX+8]=0xce2ffb0
+                mov     [ebp+var_18], 50h
+                mov     [ebx+4], edx    ; EDX=0xce2ffb0
+                push    0
+                call    _npinli         ; tracing nested maximum level (1) reached, skipping this CALL
+                pop     ecx
+                test    eax, eax        ; EAX=0
+                jnz     loc_56C11DA
+                mov     ecx, [ebp+var_14] ; [EBP-14h]=0xc98c938
+                lea     edx, [ebp+var_18] ; [EBP-18h]=0x50
+                push    edx             ; EDX=0xd76c93c
+                push    dword ptr [ebx+8] ; [EBX+8]=0xce2ffb0
+                push    dword ptr [ecx+13278h] ; [ECX+13278h]=0xacce190
+                call    _nrtnsvrs       ; tracing nested maximum level (1) reached, skipping this CALL
+                add     esp, 0Ch
+
+loc_2CE122B:                            ; CODE XREF: _kqvrow_+29E0118
+                mov     dword ptr [ebx], 4
+                jmp     loc_2CE1192
+; ---------------------------------------------------------------------------
+
+loc_2CE1236:                            ; DATA XREF: .rdata:0628B0AC
+                lea     edx, [ebp+var_7C] ; [EBP-7Ch]=1
+                push    edx             ; EDX=0xd76c8d8
+                push    0
+                mov     esi, [ebx+8]    ; [EBX+8]=0xce2ffb0, "TNS for 32-bit Windows: Version 11.2.0.1.0 - Production"
+                mov     [ebx+4], esi    ; ESI=0xce2ffb0, "TNS for 32-bit Windows: Version 11.2.0.1.0 - Production"
+                mov     ecx, 50h
+                mov     [ebp+var_18], ecx ; ECX=0x50
+                push    ecx             ; ECX=0x50
+                push    esi             ; ESI=0xce2ffb0, "TNS for 32-bit Windows: Version 11.2.0.1.0 - Production"
+                call    _lxvers         ; tracing nested maximum level (1) reached, skipping this CALL
+                add     esp, 10h
+                mov     edx, [ebp+var_18] ; [EBP-18h]=0x50
+                mov     dword ptr [ebx], 5
+                test    edx, edx        ; EDX=0x50
+                jnz     loc_2CE1192
+                mov     edx, [ebp+var_14]
+                mov     esi, [ebp+var_C]
+                mov     eax, ebx
+                mov     ebx, [ebp+var_8]
+                mov     ecx, 5
+                jmp     loc_2CE10F6
+; ---------------------------------------------------------------------------
+
+loc_2CE127A:                            ; DATA XREF: .rdata:0628B0B0
+                mov     edx, [ebp+var_14] ; [EBP-14h]=0xc98c938
+                mov     esi, [ebp+var_C] ; [EBP-0Ch]=0xcdfe248
+                mov     edi, [ebp+var_4] ; [EBP-4]=0xc98c938
+                mov     eax, ebx        ; EBX=0xcdfe554
+                mov     ebx, [ebp+var_8] ; [EBP-8]=0
+
+loc_2CE1288:                            ; CODE XREF: _kqvrow_+1F
+                mov     eax, [eax+8]    ; [EAX+8]=0xce2ffb0, "NLSRTL Version 11.2.0.1.0 - Production"
+                test    eax, eax        ; EAX=0xce2ffb0, "NLSRTL Version 11.2.0.1.0 - Production"
+                jz      short loc_2CE12A7
+                push    offset aXKqvvsnBuffer ; "x$kqvvsn buffer"
+                push    eax             ; EAX=0xce2ffb0, "NLSRTL Version 11.2.0.1.0 - Production"
+                mov     eax, [ebp+arg_C] ; [EBP+14h]=0x8a172b4
+                push    eax             ; EAX=0x8a172b4
+                push    dword ptr [edx+10494h] ; [EDX+10494h]=0xc98cd58
+                call    _kghfrf         ; tracing nested maximum level (1) reached, skipping this CALL
+                add     esp, 10h
+
+loc_2CE12A7:                            ; CODE XREF: _kqvrow_+1C1
+                xor     eax, eax
+                mov     esp, ebp
+                pop     ebp
+                retn                    ; EAX=0
+_kqvrow_        endp
