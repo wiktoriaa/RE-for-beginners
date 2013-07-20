df65297f5d56537a47af2c58662d986472d4230f
Dennis Yurichev <dennis@yurichev.com>
2013-07-21 02:44:52 +0300

    =...

---
+++ switch/few_ARM_ARM_O3.asm
@@ -0,0 +1,14 @@
+.text:0000014C             f1
+.text:0000014C 00 00 50 E3           CMP     R0, #0
+.text:00000150 13 0E 8F 02           ADREQ   R0, aZero       ; "zero\n"
+.text:00000154 05 00 00 0A           BEQ     loc_170
+.text:00000158 01 00 50 E3           CMP     R0, #1
+.text:0000015C 4B 0F 8F 02           ADREQ   R0, aOne        ; "one\n"
+.text:00000160 02 00 00 0A           BEQ     loc_170
+.text:00000164 02 00 50 E3           CMP     R0, #2
+.text:00000168 4A 0F 8F 12           ADRNE   R0, aSomethingUnkno ; "something unknown\n"
+.text:0000016C 4E 0F 8F 02           ADREQ   R0, aTwo        ; "two\n"
+.text:00000170
+.text:00000170             loc_170                           ; CODE XREF: f1+8
+.text:00000170                                               ; f1+14
+.text:00000170 78 18 00 EA           B       __2printf
---
+++ switch/few_ARM_thumb_O3.asm
@@ -0,0 +1,26 @@
+.text:000000D4              f1
+.text:000000D4 10 B5                      PUSH    {R4,LR}
+.text:000000D6 00 28                      CMP     R0, #0
+.text:000000D8 05 D0                      BEQ     zero_case
+.text:000000DA 01 28                      CMP     R0, #1
+.text:000000DC 05 D0                      BEQ     one_case
+.text:000000DE 02 28                      CMP     R0, #2
+.text:000000E0 05 D0                      BEQ     two_case
+.text:000000E2 91 A0                      ADR     R0, aSomethingUnkno ; "something unknown\n"
+.text:000000E4 04 E0                      B       default_case
+.text:000000E6              ; -------------------------------------------------------------------------
+.text:000000E6              zero_case                             ; CODE XREF: f1+4
+.text:000000E6 95 A0                      ADR     R0, aZero       ; "zero\n"
+.text:000000E8 02 E0                      B       default_case
+.text:000000EA              ; -------------------------------------------------------------------------
+.text:000000EA              one_case                              ; CODE XREF: f1+8
+.text:000000EA 96 A0                      ADR     R0, aOne        ; "one\n"
+.text:000000EC 00 E0                      B       default_case
+.text:000000EE              ; -------------------------------------------------------------------------
+.text:000000EE              two_case                              ; CODE XREF: f1+C
+.text:000000EE 97 A0                      ADR     R0, aTwo        ; "two\n"
+.text:000000F0              default_case                          ; CODE XREF: f1+10
+.text:000000F0                                                    ; f1+14
+.text:000000F0 06 F0 7E F8                BL      __2printf
+.text:000000F4 10 BD                      POP     {R4,PC}
+.text:000000F4              ; End of function f1
