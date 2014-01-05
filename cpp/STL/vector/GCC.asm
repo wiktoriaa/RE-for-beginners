8b40baaf56715a53ee3824d46f82cb93122b9d81
Dennis Yurichev <dennis@yurichev.com>
2014-01-05 13:43:11 +0200

    C++ STL std::vector; restructuring

---
+++ cpp/STL/vector/GCC.asm
@@ -0,0 +1,157 @@
+main            proc near
+                push    ebp
+                mov     ebp, esp
+                push    edi
+                push    esi
+                push    ebx
+                and     esp, 0FFFFFFF0h
+                sub     esp, 20h
+                mov     dword ptr [esp+14h], 0
+                mov     dword ptr [esp+18h], 0
+                mov     dword ptr [esp+1Ch], 0
+                lea     eax, [esp+14h]
+                mov     [esp], eax
+                call    _Z4dumpP14vector_of_ints ; dump(vector_of_ints *)
+                mov     dword ptr [esp+10h], 1
+                lea     eax, [esp+10h]
+                mov     [esp+4], eax
+                lea     eax, [esp+14h]
+                mov     [esp], eax
+                call    _ZNSt6vectorIiSaIiEE9push_backERKi ; std::vector<int,std::allocator<int>>::push_back(int  const&)
+                lea     eax, [esp+14h]
+                mov     [esp], eax
+                call    _Z4dumpP14vector_of_ints ; dump(vector_of_ints *)
+                mov     dword ptr [esp+10h], 2
+                lea     eax, [esp+10h]
+                mov     [esp+4], eax
+                lea     eax, [esp+14h]
+                mov     [esp], eax
+                call    _ZNSt6vectorIiSaIiEE9push_backERKi ; std::vector<int,std::allocator<int>>::push_back(int  const&)
+                lea     eax, [esp+14h]
+                mov     [esp], eax
+                call    _Z4dumpP14vector_of_ints ; dump(vector_of_ints *)
+                mov     dword ptr [esp+10h], 3
+                lea     eax, [esp+10h]
+                mov     [esp+4], eax
+                lea     eax, [esp+14h]
+                mov     [esp], eax
+                call    _ZNSt6vectorIiSaIiEE9push_backERKi ; std::vector<int,std::allocator<int>>::push_back(int  const&)
+                lea     eax, [esp+14h]
+                mov     [esp], eax
+                call    _Z4dumpP14vector_of_ints ; dump(vector_of_ints *)
+                mov     dword ptr [esp+10h], 4
+                lea     eax, [esp+10h]
+                mov     [esp+4], eax
+                lea     eax, [esp+14h]
+                mov     [esp], eax
+                call    _ZNSt6vectorIiSaIiEE9push_backERKi ; std::vector<int,std::allocator<int>>::push_back(int  const&)
+                lea     eax, [esp+14h]
+                mov     [esp], eax
+                call    _Z4dumpP14vector_of_ints ; dump(vector_of_ints *)
+                mov     ebx, [esp+14h]
+                mov     eax, [esp+1Ch]
+                sub     eax, ebx
+                cmp     eax, 17h
+                ja      short loc_80001CF
+                mov     edi, [esp+18h]
+                sub     edi, ebx
+                sar     edi, 2
+                mov     dword ptr [esp], 18h
+                call    _Znwj           ; operator new(uint)
+                mov     esi, eax
+                test    edi, edi
+                jz      short loc_80001AD
+                lea     eax, ds:0[edi*4]
+                mov     [esp+8], eax    ; n
+                mov     [esp+4], ebx    ; src
+                mov     [esp], esi      ; dest
+                call    memmove
+
+loc_80001AD:                            ; CODE XREF: main+F8
+                mov     eax, [esp+14h]
+                test    eax, eax
+                jz      short loc_80001BD
+                mov     [esp], eax      ; void *
+                call    _ZdlPv          ; operator delete(void *)
+
+loc_80001BD:                            ; CODE XREF: main+117
+                mov     [esp+14h], esi
+                lea     eax, [esi+edi*4]
+                mov     [esp+18h], eax
+                add     esi, 18h
+                mov     [esp+1Ch], esi
+
+loc_80001CF:                            ; CODE XREF: main+DD
+                lea     eax, [esp+14h]
+                mov     [esp], eax
+                call    _Z4dumpP14vector_of_ints ; dump(vector_of_ints *)
+                mov     dword ptr [esp+10h], 5
+                lea     eax, [esp+10h]
+                mov     [esp+4], eax
+                lea     eax, [esp+14h]
+                mov     [esp], eax
+                call    _ZNSt6vectorIiSaIiEE9push_backERKi ; std::vector<int,std::allocator<int>>::push_back(int  const&)
+                lea     eax, [esp+14h]
+                mov     [esp], eax
+                call    _Z4dumpP14vector_of_ints ; dump(vector_of_ints *)
+                mov     dword ptr [esp+10h], 6
+                lea     eax, [esp+10h]
+                mov     [esp+4], eax
+                lea     eax, [esp+14h]
+                mov     [esp], eax
+                call    _ZNSt6vectorIiSaIiEE9push_backERKi ; std::vector<int,std::allocator<int>>::push_back(int  const&)
+                lea     eax, [esp+14h]
+                mov     [esp], eax
+                call    _Z4dumpP14vector_of_ints ; dump(vector_of_ints *)
+                mov     eax, [esp+14h]
+                mov     edx, [esp+18h]
+                sub     edx, eax
+                cmp     edx, 17h
+                ja      short loc_8000246
+                mov     dword ptr [esp], offset aVector_m_range ; "vector::_M_range_check"
+                call    _ZSt20__throw_out_of_rangePKc ; std::__throw_out_of_range(char  const*)
+
+loc_8000246:                            ; CODE XREF: main+19C
+                mov     eax, [eax+14h]
+                mov     [esp+8], eax
+                mov     dword ptr [esp+4], offset aD ; "%d\n"
+                mov     dword ptr [esp], 1
+                call    __printf_chk
+                mov     eax, [esp+14h]
+                mov     eax, [eax+20h]
+                mov     [esp+8], eax
+                mov     dword ptr [esp+4], offset aD ; "%d\n"
+                mov     dword ptr [esp], 1
+                call    __printf_chk
+                mov     eax, [esp+14h]
+                test    eax, eax
+                jz      short loc_80002AC
+                mov     [esp], eax      ; void *
+                call    _ZdlPv          ; operator delete(void *)
+                jmp     short loc_80002AC
+; ---------------------------------------------------------------------------
+                mov     ebx, eax
+                mov     edx, [esp+14h]
+                test    edx, edx
+                jz      short loc_80002A4
+                mov     [esp], edx      ; void *
+                call    _ZdlPv          ; operator delete(void *)
+
+loc_80002A4:                            ; CODE XREF: main+1FE
+                mov     [esp], ebx
+                call    _Unwind_Resume
+; ---------------------------------------------------------------------------
+
+loc_80002AC:                            ; CODE XREF: main+1EA
+                                        ; main+1F4
+                mov     eax, 0
+                lea     esp, [ebp-0Ch]
+                pop     ebx
+                pop     esi
+                pop     edi
+                pop     ebp
+
+locret_80002B8:                         ; DATA XREF: .eh_frame:08000510
+                                        ; .eh_frame:080005BC
+                retn
+main            endp
