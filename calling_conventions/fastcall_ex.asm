469aa3ef56bdfc73ab93aef474845744baf700d2
Dennis Yurichev <dennis@yurichev.com>
2014-03-14 22:44:26 +0200

    calling conventions: more examples

---
+++ calling_conventions/fastcall_ex.asm
@@ -0,0 +1,20 @@
+_c$ = 8							; size = 4
+@f3@12	PROC
+; _a$ = ecx
+; _b$ = edx
+	mov	eax, ecx
+	imul	eax, edx
+	add	eax, DWORD PTR _c$[esp-4]
+	ret	4
+@f3@12	ENDP
+
+; ...
+
+	mov	edx, 2
+	push	3
+	lea	ecx, DWORD PTR [edx-1]
+	call	@f3@12
+	push	eax
+	push	OFFSET $SG81390
+	call	_printf
+	add	esp, 8
---
+++ calling_conventions/stdcall_ex.asm
@@ -0,0 +1,22 @@
+_a$ = 8							; size = 4
+_b$ = 12						; size = 4
+_c$ = 16						; size = 4
+_f2@12	PROC
+	push	ebp
+	mov	ebp, esp
+	mov	eax, DWORD PTR _a$[ebp]
+	imul	eax, DWORD PTR _b$[ebp]
+	add	eax, DWORD PTR _c$[ebp]
+	pop	ebp
+	ret	12					; 0000000cH
+_f2@12	ENDP
+
+; ...
+	push	3
+	push	2
+	push	1
+	call	_f2@12
+	push	eax
+	push	OFFSET $SG81369
+	call	_printf
+	add	esp, 8
