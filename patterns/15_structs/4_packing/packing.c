aaea6eb4449e4e7b590fe2f6ecdd9310e7ccb962
Dennis Yurichev <dennis@yurichev.com>
2014-06-05 13:44:00 +0300

    more debugger examples

--- patterns/15_structs/4_packing/packing.c
+++ patterns/15_structs/4_packing/packing.c
@@ -12,3 +12,13 @@ void f(struct s s)
 {
     printf ("a=%d; b=%d; c=%d; d=%d\n", s.a, s.b, s.c, s.d);
 };
+
+int main()
+{
+    struct s tmp;
+    tmp.a=1;
+    tmp.b=2;
+    tmp.c=3;
+    tmp.d=4;
+    f(tmp);
+};
--- patterns/15_structs/4_packing/packing_msvc_Zp1.asm
+++ patterns/15_structs/4_packing/packing_msvc_Zp1.asm
@@ -1,3 +1,27 @@
+_main    PROC
+    push   ebp
+    mov    ebp, esp
+    sub    esp, 12
+    mov    BYTE PTR _tmp$[ebp], 1
+    mov    DWORD PTR _tmp$[ebp+1], 2
+    mov    BYTE PTR _tmp$[ebp+5], 3
+    mov    DWORD PTR _tmp$[ebp+6], 4
+    sub    esp, 12
+    mov    eax, esp
+    mov    ecx, DWORD PTR _tmp$[ebp]
+    mov    DWORD PTR [eax], ecx
+    mov    edx, DWORD PTR _tmp$[ebp+4]
+    mov    DWORD PTR [eax+4], edx
+    mov    cx, WORD PTR _tmp$[ebp+8]
+    mov    WORD PTR [eax+8], cx
+    call    _f
+    add    esp, 12
+    xor    eax, eax
+    mov    esp, ebp
+    pop    ebp
+    ret    0
+_main    ENDP
+
 _TEXT    SEGMENT
 _s$ = 8 ; size = 10
 ?f@@YAXUs@@@Z PROC    ; f
