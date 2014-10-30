654881238a224fae6ba7e56c2c70627413f845fb
Dennis Yurichev <dennis@yurichev.com>
2014-10-30 11:01:18 +0200

    bugfix from Changmin Heo

--- patterns/07_jcc/minmax/minmax_GCC481_O3.s
+++ patterns/07_jcc/minmax/minmax_GCC481_O3.s
@@ -1,25 +1,23 @@
 my_max:
 	mov	edx, DWORD PTR [esp+4]
 	mov	eax, DWORD PTR [esp+8]
-; EAX=A
-; EDX=B
-; compare B and A:
+; EDX=A
+; EAX=B
+; compare A and B:
 	cmp	edx, eax
-; if B>=A, load B value in EAX
-; the instruction idle if otherwise (if B<A)
+; if A>=B, load A value into EAX
+; the instruction idle if otherwise (if A<B)
 	cmovge	eax, edx
 	ret
 
 my_min:
 	mov	edx, DWORD PTR [esp+4]
 	mov	eax, DWORD PTR [esp+8]
-; EAX=A
-; EDX=B
-; compare B and A:
+; EDX=A
+; EAX=B
+; compare A and B:
 	cmp	edx, eax
-; EAX=A
-; EDX=B
-; if B<=A, load B value in EAX
-; the instruction idle if otherwise (if B>A)
+; if A<=B, load A value into EAX
+; the instruction idle if otherwise (if A>B)
 	cmovle	eax, edx
 	ret
