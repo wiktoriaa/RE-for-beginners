f2e515c144b4913db040bfde88cedb0bbbb9fbd9
Dennis Yurichev <dennis@yurichev.com>
2015-11-25 03:04:59 +0200

    bugfix

--- advanced/102_fib/fib.asm
+++ advanced/102_fib/fib.asm
@@ -1,25 +1,31 @@
-_TEXT	SEGMENT
-_a$ = 8		; size = 4
-_b$ = 12	; size = 4
-_limit$ = 16	; size = 4
+_a$ = 8			; size = 4
+_b$ = 12		; size = 4
+_limit$ = 16		; size = 4
 _fib	PROC
 	push	ebp
 	mov	ebp, esp
 	mov	eax, DWORD PTR _a$[ebp]
 	add	eax, DWORD PTR _b$[ebp]
 	push	eax
-	push	OFFSET $SG2750 ; "%d"
+	push	OFFSET $SG2643
 	call	DWORD PTR __imp__printf
 	add	esp, 8
-	mov	ecx, DWORD PTR _limit$[ebp]
-	push	ecx
-	mov	edx, DWORD PTR _a$[ebp]
-	add	edx, DWORD PTR _b$[ebp]
+	mov	ecx, DWORD PTR _a$[ebp]
+	add	ecx, DWORD PTR _b$[ebp]
+	cmp	ecx, DWORD PTR _limit$[ebp]
+	jle	SHORT $LN1@fib
+	jmp	SHORT $LN2@fib
+$LN1@fib:
+	mov	edx, DWORD PTR _limit$[ebp]
 	push	edx
-	mov	eax, DWORD PTR _b$[ebp]
+	mov	eax, DWORD PTR _a$[ebp]
+	add	eax, DWORD PTR _b$[ebp]
 	push	eax
+	mov	ecx, DWORD PTR _b$[ebp]
+	push	ecx
 	call	_fib
 	add	esp, 12
+$LN2@fib:
 	pop	ebp
 	ret	0
 _fib	ENDP
@@ -27,7 +33,7 @@ _fib	ENDP
 _main	PROC
 	push	ebp
 	mov	ebp, esp
-	push	OFFSET $SG2753 ; "0\n1\n1\n"
+	push	OFFSET $SG2647
 	call	DWORD PTR __imp__printf
 	add	esp, 4
 	push	20
