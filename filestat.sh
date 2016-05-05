a76d577f690c188e7476ab4e914612a0b9dc37f9
Dennis Yurichev <dennis@yurichev.com>
2016-05-05 16:52:08 +0300

    Portuguese translation by Thales Stevan de A. Gois

---
+++ filestat.sh
@@ -0,0 +1,15 @@
+echo tex
+find . -name '*.tex' | wc -l
+echo c
+find . -name '*.c' | wc -l
+echo cpp
+find . -name '*.cpp' | wc -l
+echo lst
+find . -name '*.lst' | wc -l
+echo asm
+find . -name '*.asm' | wc -l
+echo s
+find . -name '*.s' | wc -l
+echo png
+find . -name '*.png' | wc -l
+
