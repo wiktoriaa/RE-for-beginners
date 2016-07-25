bd2e3e51a2be112d65f3d981a3e34c39f8b0c6bc
Dennis Yurichev <dennis@yurichev.com>
2016-07-25 23:21:08 +0300

    ...

--- filestat.sh
+++ filestat.sh
@@ -1,15 +1,24 @@
 echo tex
 find . -name '*.tex' | wc -l
+
 echo c
 find . -name '*.c' | wc -l
+
 echo cpp
 find . -name '*.cpp' | wc -l
+
 echo lst
 find . -name '*.lst' | wc -l
+
 echo asm
 find . -name '*.asm' | wc -l
+
 echo s
 find . -name '*.s' | wc -l
+
 echo png
 find . -name '*.png' | wc -l
+
+echo lines in all TeX files:
+find . -name '*.tex' | xargs wc -l
 
