4d7a5a76a6af147417737455e4036f22e427f466
Dennis Yurichev <dennis@yurichev.com>
2016-07-26 14:54:49 +0300

    restructuring

---
+++ patterns/14_bitfields/4_popcnt/ARM64_GCC_O0_RU.s
@@ -0,0 +1,37 @@
+f:
+	sub	sp, sp, #32
+	str	x0, [sp,8]	; §сохранить значение "a" в§ Register Save Area
+	str	wzr, [sp,24]	; rt=0
+	str	wzr, [sp,28]	; i=0
+	b	.L2
+.L4:
+	ldr	w0, [sp,28]
+	mov	x1, 1
+	lsl	x0, x1, x0	; X0 = X1<<X0 = 1<<i
+	mov	x1, x0
+; X1 = 1<<1
+	ldr	x0, [sp,8]
+; X0 = a
+	and	x0, x1, x0
+; X0 = X1&X0 = (1<<i) & a
+; X0 §содержит ноль§? §тогда перейти на .L3, пропуская инкремент "rt"§
+	cmp	x0, xzr
+	beq	.L3
+; rt++
+	ldr	w0, [sp,24]
+	add	w0, w0, 1
+	str	w0, [sp,24]
+.L3:
+; i++
+	ldr	w0, [sp,28]
+	add	w0, w0, 1
+	str	w0, [sp,28]
+.L2:
+; i<=63? §тогда перейти на§ .L4
+	ldr	w0, [sp,28]
+	cmp	w0, 63
+	ble	.L4
+; §возврат§ rt
+	ldr	w0, [sp,24]
+	add	sp, sp, 32
+	ret
---
+++ patterns/14_bitfields/4_popcnt/shifts64_GCC_O0_RU.s
@@ -0,0 +1,32 @@
+f:
+	push	rbp
+	mov	rbp, rsp
+	mov	QWORD PTR [rbp-24], rdi	; a
+	mov	DWORD PTR [rbp-12], 0	; rt=0
+	mov	QWORD PTR [rbp-8], 0	; i=0
+	jmp	.L2
+.L4:
+	mov	rax, QWORD PTR [rbp-8]
+	mov	rdx, QWORD PTR [rbp-24]
+; RAX = i, RDX = a
+	mov	ecx, eax
+; ECX = i
+	shr	rdx, cl
+; RDX = RDX>>CL = a>>i
+	mov	rax, rdx
+; RAX = RDX = a>>i
+	and	eax, 1
+; EAX = EAX&1 = (a>>i)&1
+	test	rax, rax
+; §последний бит был нулевым§?
+; §пропустить следующую инструкцию ADD, если это было так§.
+	je	.L3	
+	add	DWORD PTR [rbp-12], 1	; rt++
+.L3:
+	add	QWORD PTR [rbp-8], 1	; i++
+.L2:
+	cmp	QWORD PTR [rbp-8], 63	; i<63?
+	jbe	.L4			; §перейти на начало тела цикла, если это так§
+	mov	eax, DWORD PTR [rbp-12]	; §возврат§ rt
+	pop	rbp
+	ret
