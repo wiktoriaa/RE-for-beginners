961318628f28817f9d640b453766d583e150df2c
Dennis Yurichev <dennis@yurichev.com>
2016-08-22 23:47:33 +0300

    restructuring

---
+++ patterns/13_arrays/5_multidimensional/2D_as_1D_GCC49_x64_O3_EN.s
@@ -0,0 +1,33 @@
+; RDI=§§address of array§§
+; RSI=a
+; RDX=b
+
+get_by_coordinates1:
+; §sign-extend input 32-bit int values "a" and "b" to 64-bit ones§
+	movsx	rsi, esi 
+	movsx	rdx, edx
+	lea	rax, [rdi+rsi*4]
+; RAX=RDI+RSI*4=§address of array§+a*4
+	movzx	eax, BYTE PTR [rax+rdx]
+; AL=§load byte at address§ RAX+RDX=§address of array§+a*4+b
+	ret
+
+get_by_coordinates2:
+	lea	eax, [rdx+rsi*4]
+; RAX=RDX+RSI*4=b+a*4
+	cdqe
+	movzx	eax, BYTE PTR [rdi+rax]
+; AL=§load byte at address§ RDI+RAX=§address of array§+b+a*4
+	ret
+
+get_by_coordinates3:
+	sal	esi, 2
+; ESI=a<<2=a*4
+; §sign-extend input 32-bit int values "a*4" and "b" to 64-bit ones§
+	movsx	rdx, edx
+	movsx	rsi, esi
+	add	rdi, rsi
+; RDI=RDI+RSI=§address of array§+a*4
+	movzx	eax, BYTE PTR [rdi+rdx]
+; AL=§load byte at address§ RDI+RDX=§address of array§+a*4+b
+	ret
---
+++ patterns/13_arrays/5_multidimensional/2D_as_1D_MSVC_2013_Ox_x64_RU.asm
@@ -0,0 +1,39 @@
+array$ = 8
+a$ = 16
+b$ = 24
+get_by_coordinates3 PROC
+; RCX=§адрес массива§
+; RDX=a
+; R8=b
+	movsxd	rax, r8d
+; EAX=b
+	movsxd	r9, edx
+; R9=a
+	add	rax, rcx
+; RAX=b+§адрес массива§
+	movzx	eax, BYTE PTR [rax+r9*4]
+; AL=§загрузить байт по адресу§ RAX+R9*4=b+§адрес массива§+a*4=§адрес массива§+a*4+b
+	ret	0
+get_by_coordinates3 ENDP
+
+array$ = 8
+a$ = 16
+b$ = 24
+get_by_coordinates2 PROC
+	movsxd	rax, r8d
+	movsxd	r9, edx
+	add	rax, rcx
+	movzx	eax, BYTE PTR [rax+r9*4]
+	ret	0
+get_by_coordinates2 ENDP
+
+array$ = 8
+a$ = 16
+b$ = 24
+get_by_coordinates1 PROC
+	movsxd	rax, r8d
+	movsxd	r9, edx
+	add	rax, rcx
+	movzx	eax, BYTE PTR [rax+r9*4]
+	ret	0
+get_by_coordinates1 ENDP
