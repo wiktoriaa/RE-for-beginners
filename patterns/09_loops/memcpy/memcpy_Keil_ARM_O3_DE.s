6fa72369de3502bf1445938138a172b8964a9344
Julius Angres <julius.angres@outlook.de>
2016-12-28 20:45:11 +0100

    German translations

---
+++ patterns/09_loops/memcpy/memcpy_Keil_ARM_O3_DE.s
@@ -0,0 +1,25 @@
+my_memcpy PROC
+; R0 = Zieladresse
+; R1 = Quelladresse
+; R2 = Blockgröße
+
+; initialisiere Zähler (i) mit 0
+        MOV      r3,#0
+|L0.4|
+; alles kopiert?
+        CMP      r3,r2
+; der folgende Codeblock wird nur ausgeführt, falls die Â§\IT{less than}Â§
+Bedingung, ; d.h., falls R2<R3 oder i<size wahr ist.
+; lade Byte von R1+i:
+        LDRBCC   r12,[r1,r3]
+; speichere Byte an der Stelle R1+i:
+        STRBCC   r12,[r0,r3]
+; i++
+        ADDCC    r3,r3,#1
+; der letzte Befehl des Â§\IT{conditional block}Â§.
+; springe zum Anfang der Schleife, falls i<size
+; andernfalls tue nichts (d.h., falls i>=size)
+        BCC      |L0.4|
+; return
+        BX       lr
+        ENDP
