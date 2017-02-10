6bab4f2874342f1776f265c4d817a75d129d9c71
Julius Angres <julius.angres@outlook.de>
2017-02-10 15:00:43 +0100

    German translations

---
+++ patterns/13_arrays/2_BO/w_DE.asm
@@ -0,0 +1,26 @@
+_TEXT    SEGMENT
+_i$ = -84 ; size = 4
+_a$ = -80 ; size = 80
+_main   PROC
+ push   ebp
+ mov    ebp, esp
+ sub    esp, 84
+ mov    DWORD PTR _i$[ebp], 0
+ jmp    SHORT $LN3@main
+$LN2@main:
+ mov    eax, DWORD PTR _i$[ebp]
+ add    eax, 1
+ mov    DWORD PTR _i$[ebp], eax
+$LN3@main:
+ cmp    DWORD PTR _i$[ebp], 30 ; 0000001eH
+ jge    SHORT $LN1@main
+ mov    ecx, DWORD PTR _i$[ebp]  
+ mov    edx, DWORD PTR _i$[ebp]      ; dieser Befehl ist offensichtlich redundant
+ mov    DWORD PTR _a$[ebp+ecx*4], edx ; ECX könnte hier als zweiter Operand verwendet werden
+ jmp    SHORT $LN2@main
+$LN1@main:
+ xor    eax, eax
+ mov    esp, ebp
+ pop    ebp
+ ret    0
+_main   ENDP
