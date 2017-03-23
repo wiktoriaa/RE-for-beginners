1e73d6cd4c22264025f813f8aa74eb8b706ed5ed
Dennis Yurichev <dennis@yurichev.com>
2017-03-24 01:27:13 +0200

    ...

---
+++ fundamentals/data_types_and_numbers_EN_lst1.c
@@ -0,0 +1,4 @@
+	if (1234)
+		printf ("this will always be executed\n");
+	else
+		printf ("this will never\n");
---
+++ fundamentals/data_types_and_numbers_EN_lst2.c
@@ -0,0 +1,7 @@
+	char *input=...;
+
+	while(*input) // execute body if *input character is non-zero
+	{
+		// do something with *input
+		input++;
+	};
