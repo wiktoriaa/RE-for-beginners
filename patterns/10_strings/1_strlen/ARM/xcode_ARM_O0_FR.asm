f0f6073113239575680676aa6907cf0622d72e3f
Marc Remy <mremy@gmx.ch>
2017-06-08 14:10:09 +0200

    French: patterns/10_strings

---
+++ patterns/10_strings/1_strlen/ARM/xcode_ARM_O0_FR.asm
@@ -0,0 +1,25 @@
+_strlen
+
+eos  = -8
+str  = -4
+
+     SUB    SP, SP, #8 ; allouer 8 octets pour les variables locales
+     STR    R0, [SP,#8+str]
+     LDR    R0, [SP,#8+str]
+     STR    R0, [SP,#8+eos]
+
+loc_2CB8 ; CODE XREF: _strlen+28
+     LDR    R0, [SP,#8+eos]
+     ADD    R1, R0, #1
+     STR    R1, [SP,#8+eos]
+     LDRSB  R0, [R0]
+     CMP    R0, #0
+     BEQ    loc_2CD4
+     B      loc_2CB8
+loc_2CD4 ; CODE XREF: _strlen+24
+     LDR    R0, [SP,#8+eos]
+     LDR    R1, [SP,#8+str]
+     SUB    R0, R0, R1 ; R0=eos-str
+     SUB    R0, R0, #1 ; R0=R0-1
+     ADD    SP, SP, #8 ; §libérer les 8 octets alloués§
+     BX     LR
