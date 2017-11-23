4dc1fe6f69d602b847c4ad79369299792d900065
Marc Remy <mremy@gmx.ch>
2017-11-23 22:12:50 +0100

    French: remove non-ASCII characters from C code
    To avoid issue with encoding.
    Add a footnote to warn about this.

--- patterns/13_arrays/45_month_1D/month1_FR.c
+++ patterns/13_arrays/45_month_1D/month1_FR.c
@@ -2,9 +2,9 @@
 
 const char* month1[]=
 {
-	"janvier", "février", "mars", "avril",
-	"mai", "juin", "juillet", "août",
-	"septembre", "octobre", "novembre", "décembre"
+	"janvier", "fevrier", "mars", "avril",
+	"mai", "juin", "juillet", "aout",
+	"septembre", "octobre", "novembre", "decembre"
 };
 
 // dans l'intervalle 0..11
