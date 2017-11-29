c13c9d4caafdba494e0c1655dcf2cee71b7bba88
BlueSkeye <bsgithub@gmail.com>
2017-11-29 21:20:51 +0100

    FR : patterns\structs intermediate commit

---
+++ patterns/15_structs/3_tm_linux/GCC_tm_FR.asm
@@ -0,0 +1,47 @@
+﻿main proc near
+     push    ebp
+     mov     ebp, esp
+     and     esp, 0FFFFFFF0h
+     sub     esp, 40h
+     mov     dword ptr [esp], 0 ; premier argument de la fonction time()
+     call    time
+     mov     [esp+3Ch], eax
+     lea     eax, [esp+3Ch]  ; récupération de la valeur retournée par time()
+     lea     edx, [esp+10h]  ; la structure tm est à l'adresse ESP+10h
+     mov     [esp+4], edx    ; passons le pointeur vers la structure begin
+     mov     [esp], eax      ; ... et le pointeur retourné par time()
+     call    localtime_r
+     mov     eax, [esp+24h]  ; tm_year
+     lea     edx, [eax+76Ch] ; edx=eax+1900
+     mov     eax, offset format ; "Year: %d\n"
+     mov     [esp+4], edx
+     mov     [esp], eax
+     call    printf
+     mov     edx, [esp+20h]      ; tm_mon
+     mov     eax, offset aMonthD ; "Month: %d\n"
+     mov     [esp+4], edx
+     mov     [esp], eax
+     call    printf
+     mov     edx, [esp+1Ch]     ; tm_mday
+     mov     eax, offset aDayD  ; "Day: %d\n"
+     mov     [esp+4], edx
+     mov     [esp], eax
+     call    printf
+     mov     edx, [esp+18h]     ; tm_hour
+     mov     eax, offset aHourD ; "Hour: %d\n"
+     mov     [esp+4], edx
+     mov     [esp], eax
+     call    printf
+     mov     edx, [esp+14h]        ; tm_min
+     mov     eax, offset aMinutesD ; "Minutes: %d\n"
+     mov     [esp+4], edx
+     mov     [esp], eax
+     call    printf
+     mov     edx, [esp+10h]
+     mov     eax, offset aSecondsD ; "Seconds: %d\n"
+     mov     [esp+4], edx          ; tm_sec
+     mov     [esp], eax
+     call    printf
+     leave
+     retn
+main endp
