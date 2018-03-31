e6fa55dfdd5250717dff31ca4884c9daeed60b08
Marc Remy <mremy@gmx.ch>
2018-03-31 09:47:32 +0200

    French: corrections

--- patterns/15_structs/6_bitfields/float/float_gcc_O3_FR.asm
+++ patterns/15_structs/6_bitfields/float/float_gcc_O3_FR.asm
@@ -12,7 +12,7 @@ arg_0  = dword ptr  8
        or      eax, 80000000h  ; positionnement du signe négatif
        mov     edx, eax
        and     eax, 807FFFFFh  ; Nous ne conservons que le signe et le signifiant dans EAX
-       shr     edx, 23         ; Preparation de l'exposant
+       shr     edx, 23         ; Préparation de l'exposant
        add     edx, 2          ; Ajout de 2
        movzx   edx, dl         ; RAZ de tous les octets dans EAX à l'exception des bits 7:0
        shl     edx, 23         ; Décalage du nouvel exposant pour qu'ils soit à sa place
